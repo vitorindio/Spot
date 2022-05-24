033d56bdad3d1c40c7aad4cfa0cfef6124647538
Arnon <arnonrdp@gmail.com>
2022-05-24 07:06:51 -0300

    Adicionadas cores da OWSE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'owse-blue': '#0F6DB8',
+        'owse-red': '#EB5723'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
