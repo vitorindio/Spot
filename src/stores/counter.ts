7acfada42d2acd2b369bced270c948cd62f21b38
Arnon <arnonrdp@gmail.com>
2022-06-02 10:55:32 -0300

    Pinia stores

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 // useStore could be anything like useUser, useCart
 // the first argument is a unique id of the store across your application
-export const useCounter = defineStore('main', {
+export const useCounterStore = defineStore('counter', {
   state: () => ({
     counter: 0
   }),
