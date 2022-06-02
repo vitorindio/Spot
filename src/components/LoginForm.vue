b22e28859858813f8777cd8341ef90bf8d34cea7
Arnon Rodrigues <arnonrdp@gmail.com>
2022-06-02 16:21:33 -0300

    Login Refatorado

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,56 +1,19 @@
 <template>
-  <section class="text-gray-600 body-font bg-gray-100">
-    <div class="container h-screen xl:px-32 p-16 py-48 mx-auto flex flex-wrap justify-center items-center">
-      <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-        <img src="@/assets/logotipo.svg" class="mx-auto md:ml-0" alt="Logotipo OWSE" />
-        <p class="leading-relaxed mt-4 lg:text-3xl text-2xl lg:max-w-xl font-medium text-black text-center md:text-left">
-          SPOT vai te ajudar a controlar suas horas de trabalho.
-        </p>
-      </div>
-      <form
-        @submit.prevent="store.login(state.email, state.password)"
-        class="lg:w-2/5 md:w-1/2 bg-white shadow-lg rounded-lg p-8 flex flex-col w-full"
-      >
-        <img src="@/assets/spot.svg" class="mx-auto mb-8 w-2/5" alt="Logotipo SPOT" />
-        <div class="relative mb-4">
-          <input
-            name="email"
-            placeholder="E-mail"
-            class="w-full bg-white rounded border border-gray-300 focus:ring-2 focus:ring-owse-blue text-lg outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            required
-            v-model="state.email"
-          />
-          <span v-if="v$.email.$error">
-            {{ v$.email.$errors[0].$message }}
-          </span>
-        </div>
-        <div class="relative mb-4">
-          <input
-            type="password"
-            name="password"
-            placeholder="Senha"
-            class="w-full bg-white rounded border border-gray-300 focus:ring-2 focus:ring-owse-blue outline-none text-lg text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            required
-            v-model="state.password"
-          />
-          <span v-if="v$.password.$error">
-            {{ v$.password.$errors[0].$message }}
-          </span>
-        </div>
-        <button type="submit" class="text-white border-0 py-2 px-8 focus:outline-none font-medium rounded text-xl bg-owse-blue">
-          Entrar
-        </button>
-        <p class="text-sm text-owse-blue mt-3 text-center py-4">Esqueceu a senha?</p>
-      </form>
-    </div>
-  </section>
+  <form
+    @submit.prevent="store.login(state.email, state.password)"
+    class="lg:w-2/5 md:w-1/2 bg-white shadow-lg rounded-lg p-8 flex flex-col w-full q-gutter-md"
+  >
+    <img src="@/assets/spot.svg" class="mx-auto mb-8 w-2/5" alt="Logotipo SPOT" />
+    <q-input filled v-model="state.email" label="E-mail" name="email" required :rules="mailRules" lazy-rules />
+    <q-input filled v-model="state.password" type="password" label="Senha" name="password" required :rules="passwordRules" lazy-rules />
+    <q-btn type="submit" color="primary" label="Entrar" />
+    <p class="text-sm text-owse-blue mt-3 text-center py-4">Esqueceu a senha?</p>
+  </form>
 </template>
 
 <script lang="ts">
 import { useUserStore } from '@/stores/user'
-import useValidate from '@vuelidate/core'
-import { email, helpers, minLength, required } from '@vuelidate/validators'
-import { computed, defineComponent, reactive } from 'vue'
+import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   name: 'LoginForm',
@@ -62,22 +25,12 @@ export default defineComponent({
       password: ''
     })
 
-    const mustBeOwse = (value: string) => value.includes('owse.com.br')
-
-    const rules = computed(() => {
-      return {
-        email: {
-          required,
-          email,
-          mustBeOwse: helpers.withMessage('Utilize um email OWSE', mustBeOwse)
-        },
-        password: { required, minLength: minLength(3) }
-      }
-    })
-
-    const v$ = useValidate(rules, state)
-
-    return { store, state, v$ }
+    return {
+      store,
+      state,
+      mailRules: [(val: string) => val.includes('@owse.com.br') || 'Utilize um e-mail OWSE'],
+      passwordRules: [(val: string) => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']
+    }
   }
 })
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    <LoginForm />
+    <section class="text-gray-600 body-font bg-gray-100">
+      <div class="container h-screen xl:px-32 p-16 py-48 mx-auto flex flex-wrap justify-center items-center">
+        <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <img src="@/assets/logotipo.svg" class="mx-auto md:ml-0" alt="Logotipo OWSE" />
+          <p class="leading-relaxed mt-4 lg:text-3xl text-2xl lg:max-w-xl font-medium text-black text-center md:text-left">
+            SPOT vai te ajudar a controlar suas horas de trabalho.
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+    </section>
   </div>
 </template>
 
