785cfad00c6ffe5e96c2ad2f8b8a517d75bc5cfa
Arnon <arnonrdp@gmail.com>
2022-06-03 17:35:06 -0300

    Ajustes  na posição do logo

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,10 @@
 <template>
-  <section class="bg-grey-11">
-    <div class="content-center items-center justify-center row window-height">
-      <div class="col-xs-11 col-sm-9 col-md-7 q-ma-md q-gutter-lg">
-        <img src="@/assets/logotipo.svg" :class="`${$q.screen.md ? 'q-mr-auto' : 'q-mx-auto'}`" alt="Logotipo OWSE" />
-        <h2 :class="`text-weight-medium ${$q.screen.md ? 'text-h4 text-left' : 'text-h5 text-center'}`">
-          SPOT vai te ajudar a controlar suas horas de trabalho.
-        </h2>
-      </div>
-      <div class="col-xs-11 col-sm-9 col-md-4 q-ma-md"><LoginForm /></div>
+  <section class="content-center items-center justify-center row window-height">
+    <div :class="`col-xs-11 col-sm-9 col-md-7 q-ma-md ${$q.screen.md ? 'text-left' : 'text-center'}`">
+      <img src="@/assets/logotipo.svg" :class="``" alt="Logotipo OWSE" />
+      <h2 :class="`text-weight-medium ${$q.screen.md ? 'text-h4' : 'text-h5'}`">SPOT vai te ajudar a controlar suas horas de trabalho.</h2>
     </div>
+    <div class="col-xs-11 col-sm-9 col-md-4 q-ma-md"><LoginForm /></div>
   </section>
 </template>
 
