08cd56e10e57652fe39dcac1e46c8c3fcf3880c7
Arnon <arnonrdp@gmail.com>
2022-06-03 17:13:00 -0300

    Definido background padrão para tema light

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.body--light {
+  background-color: var(--q-light);
 }
 </style>
