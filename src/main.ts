a5018f98d0456a1faedbba704068913ca05c3779
Arnon <arnonrdp@gmail.com>
2022-06-03 11:31:27 -0300

    Removido `tailwind`

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
 import quasarUserOptions from './quasar-user-options'
 import router from './router'
 
