b0bf1ca9a8a46fd00b26f902594460b427bc19a2
Arnon <arnonrdp@gmail.com>
2022-06-04 07:04:24 -0300

    Ajustes de tamanho de tela

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,25 @@
 <template>
   <section class="content-center items-center justify-center row window-height">
-    <div :class="`col-xs-11 col-sm-9 col-md-7 q-ma-md ${$q.screen.md ? 'text-left' : 'text-center'}`">
+    <div :class="`col-xs-11 col-sm-9 col-md-7 col-lg-5 col-xl-4 q-ma-md ${isScreenBiggerThanMedium ? 'text-left' : 'text-center'}`">
       <img src="@/assets/logotipo.svg" :class="``" alt="Logotipo OWSE" />
-      <h2 :class="`text-weight-medium ${$q.screen.md ? 'text-h4' : 'text-h5'}`">SPOT vai te ajudar a controlar suas horas de trabalho.</h2>
+      <h2 :class="`text-weight-medium ${isScreenBiggerThanMedium ? 'text-h4' : 'text-h5'}`">
+        SPOT vai te ajudar a controlar suas horas de trabalho.
+      </h2>
     </div>
-    <div class="col-xs-11 col-sm-9 col-md-4 q-ma-md"><LoginForm /></div>
+    <div class="col-xs-11 col-sm-9 col-md-4 col-lg-3 col-xl-2 q-ma-md"><LoginForm /></div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
 import LoginForm from '@/components/LoginForm.vue'
-import StatusService from '@/services/status'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   components: { LoginForm },
-  async mounted() {
-    const data = await StatusService.check()
-    console.log(data)
+  methods: {
+    isScreenBiggerThanMedium(): boolean {
+      return this.$q.screen.width >= this.$q.screen.sizes.md
+    }
   }
 })
 </script>
