d74dcce8a674bde203ca83b74e8ecd74fc47c977
Vindio <vitorr.brasil20@gmail.com>
2022-06-06 17:55:31 -0300

    retirada codigo unused

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,6 @@
 <template>
   <section>
     <h1>Dashboard</h1>
-    <h2 class="fontSize">Dashboard</h2>
-    <router-link to="/">Dashboard</router-link>
-    |
-    <router-link to="/about">About</router-link>
-    |
-    <router-link to="/login" @click="store.logout()">Logout</router-link>
-    <br />
-    <br />
     <code>v-if="admin"</code>
     <p>Componente Tabela do Administrador</p>
     <AdminDash />
@@ -19,7 +11,6 @@
 
 <script lang="ts">
 import AdminDash from '@/components/AdminDash.vue'
-import { useAuthStore } from '@/stores/auth'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
