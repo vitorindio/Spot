4c24955d0ec5f44d03b314f6924a988413a9738e
Arnon Rodrigues <arnonrdp@gmail.com>
2022-06-06 14:13:22 -0300

    Correções nos formularios de login e forgotPassword

--- src/components/ForgotPasswordForm.vue
+++ src/components/ForgotPasswordForm.vue
@@ -1,24 +1,74 @@
 <template>
-  <q-form @submit.prevent="this.store.forgotPassword(this.email)" class="bg-white q-gutter-y-sm q-pa-lg">
+  <q-form autofocus @submit.prevent="onSubmit" class="bg-white q-gutter-y-sm q-pa-lg">
     <img src="@/assets/spot.svg" alt="Logotipo SPOT" class="q-mx-auto q-mb-lg q-px-xl" style="height: 40px" />
-    <q-input filled v-model="email" type="email" label="E-mail" name="email" required :rules="mailRules" lazy-rules />
-    <q-btn label="Recuperar" type="submit" color="primary" class="full-width q-mb-sm" />
-    <router-link to="/login" size="sm">Voltar para Login</router-link>
+    <q-input
+      filled
+      v-model="store.email"
+      type="email"
+      label="E-mail"
+      :rules="[(val) => val.includes('@owse.com.br') || 'Utilize um e-mail OWSE']"
+      lazy-rules
+      required
+      :disable="userKey ? true : false"
+      :readonly="userKey ? true : false"
+    />
+    <q-input
+      v-show="userKey"
+      filled
+      v-model="password"
+      :type="isPwd ? 'password' : 'text'"
+      label="Nova senha"
+      :rules="[(val) => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']"
+      lazy-rules
+    >
+      <template v-slot:append>
+        <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+      </template>
+    </q-input>
+    <q-input
+      v-show="userKey"
+      filled
+      v-model="passwordConfirmation"
+      :type="isPwd ? 'password' : 'text'"
+      label="Confirmação"
+      :rules="[(val) => val === store.password || 'As senhas não conferem']"
+      lazy-rules
+    />
+    <q-btn type="submit" :label="userKey ? 'Redefinir' : 'Recuperar'" color="primary" class="full-width q-mb-sm" />
+    <router-link v-show="!userKey" userKey to="/login" size="sm">Voltar para Login</router-link>
   </q-form>
 </template>
 
 <script lang="ts">
 import { useForgotPasswordStore } from '@/stores'
+import { storeToRefs } from 'pinia'
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  setup() {
+  props: {
+    userKey: { type: String, required: false }
+  },
+
+  setup(props) {
     const store = useForgotPasswordStore()
 
-    return {
-      store,
-      email: ref(''),
-      mailRules: [(val: string) => val.includes('@owse.com.br') || 'Utilize um e-mail OWSE']
+    const isPwd = ref(true)
+    const { email, password, passwordConfirmation } = storeToRefs(store)
+
+    function onSubmit() {
+      if (props.userKey) {
+        store.update(props.userKey, store.password, store.passwordConfirmation)
+      } else {
+        store.forgotPassword(store.email)
+      }
+    }
+
+    return { store, email, password, passwordConfirmation, isPwd, onSubmit }
+  },
+
+  mounted() {
+    if (this.userKey) {
+      this.store.show(this.userKey)
     }
   }
 })
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-form @submit.prevent="this.store.login(this.email, this.password)" class="bg-white q-gutter-y-sm q-pa-lg">
+  <q-form @submit.prevent="this.store.login(this.email, this.password)" autofocus class="bg-white q-gutter-y-sm q-pa-lg">
     <img src="@/assets/spot.svg" alt="Logotipo SPOT" class="q-mx-auto q-mb-lg q-px-xl" style="height: 40px" />
     <q-input filled v-model="email" type="email" label="E-mail" name="email" required :rules="mailRules" lazy-rules />
     <q-input
@@ -23,20 +23,20 @@
 
 <script lang="ts">
 import { useAuthStore } from '@/stores'
-import { defineComponent, ref } from 'vue'
 
-export default defineComponent({
+export default {
   setup() {
     const store = useAuthStore()
 
-    return {
-      store,
-      email: ref(''),
-      password: ref(''),
-      isPwd: ref(true),
-      mailRules: [(val: string) => val.includes('@owse.com.br') || 'Utilize um e-mail OWSE'],
-      passwordRules: [(val: string) => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']
-    }
-  }
-})
+    return { store }
+  },
+
+  data: () => ({
+    email: '',
+    password: '',
+    isPwd: true,
+    mailRules: [(val: string) => val.includes('@owse.com.br') || 'Utilize um e-mail OWSE'],
+    passwordRules: [(val: string) => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']
+  })
+}
 </script>
