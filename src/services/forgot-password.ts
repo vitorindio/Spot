e8f8244a485ebdac22c5ad27e0d4b0c1c77f188c
Arnon <arnonrdp@gmail.com>
2022-06-05 21:56:28 -0300

    Service de forgot-password

--- src/services/forgot-password.ts
+++ src/services/forgot-password.ts
@@ -3,24 +3,30 @@ import axios from 'axios'
 const baseURL = `https://spot-api.up.railway.app`
 
 export default class ForgotPasswordService {
-  public async register(email: string) {
-    return await axios
-      .post(`${baseURL}/users/forgot-password`, { redirectUrl: 'https://spot-owse.vercel.app/forgot-password', email })
-      .then((response) => response.data)
-      .catch((error) => error.response.data)
+  public static register(email: string) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(`${baseURL}/users/forgot-password`, { redirectUrl: 'https://spot-owse.vercel.app/forgot-password', email })
+        .then((response) => resolve(response.data))
+        .catch((error) => reject(error.response.data))
+    })
   }
 
-  public async show(key: string) {
-    return await axios
-      .get(`${baseURL}/users/forgot-password/${key}`)
-      .then((response) => response.data)
-      .catch((error) => error.response.data)
+  public static show(key: string) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${baseURL}/users/forgot-password/${key}`)
+        .then((response) => resolve(response.data))
+        .catch((error) => reject(error.response.data))
+    })
   }
 
-  public async update(key: string, password?: string, passwordConfirmation?: string) {
-    return await axios
-      .put(`${baseURL}/users/forgot-password`, { key, password, passwordConfirmation })
-      .then((response) => response.data)
-      .catch((error) => error.response.data)
+  public static update(key: string, password?: string, passwordConfirmation?: string) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(`${baseURL}/users/forgot-password`, { key, password, passwordConfirmation })
+        .then((response) => resolve(response.data))
+        .catch((error) => reject(error.response.data))
+    })
   }
 }
