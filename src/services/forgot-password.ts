1fa643ef6f2ab018899a592d221a58b7fea22efc
Arnon Rodrigues <arnonrdp@gmail.com>
2022-06-06 14:12:08 -0300

    Tipagem e Correção no Service

--- src/services/forgot-password.ts
+++ src/services/forgot-password.ts
@@ -6,14 +6,14 @@ export default class ForgotPasswordService {
   public static register(email: string) {
     return new Promise((resolve, reject) => {
       axios
-        .post(`${baseURL}/users/forgot-password`, { redirectUrl: 'https://spot-owse.vercel.app/forgot-password', email })
+        .post(`${baseURL}/users/forgot-password`, { email, redirectUrl: 'https://spot-owse.vercel.app/forgot-password' })
         .then((response) => resolve(response.data))
         .catch((error) => reject(error.response.data))
     })
   }
 
   public static show(key: string) {
-    return new Promise((resolve, reject) => {
+    return new Promise<never>((resolve, reject) => {
       axios
         .get(`${baseURL}/users/forgot-password/${key}`)
         .then((response) => resolve(response.data))
@@ -21,7 +21,7 @@ export default class ForgotPasswordService {
     })
   }
 
-  public static update(key: string, password?: string, passwordConfirmation?: string) {
+  public static update(key: string, password: string, passwordConfirmation: string) {
     return new Promise((resolve, reject) => {
       axios
         .put(`${baseURL}/users/forgot-password`, { key, password, passwordConfirmation })
